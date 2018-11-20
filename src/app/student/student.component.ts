079e0bf9bcfde95da95db08016dc558142dd3e69
mustafa kunwa <Mustafa.kunwa@saifsys.com>
2018-11-20 18:47:53 +0530

    Date Picker

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css'],
- 
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'ja-JP'},
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adapter: DateAdapter<any>) { }
 
-  ngOnInit() {  }
+  ngOnInit() { 
+    this.adapter.setLocale('en-GB');
+   }
 
 }
