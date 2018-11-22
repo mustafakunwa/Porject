c25df051e8cb9b4423749579bf2d86dc102f0e1a
mustafa kunwa <Mustafa.kunwa@saifsys.com>
2018-11-22 14:58:40 +0530

    Validation

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,10 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {Info} from '../StudentInfo'
-import {StudentService} from '../student.service'
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter
+} from '@angular/material-moment-adapter';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from '@angular/material/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import {
+  ErrorStateMatcher
+} from '@angular/material/core';
+import {
+  Info
+} from '../StudentInfo'
+import {
+  StudentService
+} from '../student.service'
+
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -13,38 +36,60 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
-
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css'],
-  providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'ja-JP'},
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  providers: [{
+      provide: MAT_DATE_LOCALE,
+      useValue: 'ja-JP'
+    },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE]
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: MAT_MOMENT_DATE_FORMATS
+    },
   ],
 })
 export class StudentComponent implements OnInit {
 
-  public student:Info = new Info();
+  public student: Info = new Info();
 
-  constructor(private adapter: DateAdapter<any>,private StudentService:StudentService) { }
+  constructor(private adapter: DateAdapter < any > , private StudentService: StudentService, private formBuilder: FormBuilder) {}
 
-  ngOnInit() { 
+  ngOnInit() {
     this.adapter.setLocale('en-GB');
-   }
+  }
 
-   Reset():void{
-     this.student=new Info();
-   }
-   submit():void{
-     
-     this.StudentService.InsertStudent(this.student).subscribe(
-     student=>{ 
-       this.Reset()
-      },
-     error=>alert(error.Message)
-     );
+  Reset(): void {
+    this.student = new Info();
+  }
+
+  //Create a form
+  StudentForm = this.formBuilder.group({
+    FirstName: ['', Validators.required],
+    LastName: ['', Validators.required],
+    Mobile: ['', [Validators.required, ]],
+    emailFormControl: ['', [Validators.required, Validators.email]],
+    DOB: ['', [Validators.required]],
+    Gender: ['', [Validators.required]],
+    Address: ['', [Validators.required]]
+  });
+
+  submit(): void {
+    if (this.StudentForm) {
+
+      this.StudentService.InsertStudent(this.student).subscribe(
+        student => {
+          this.Reset()
+        },
+        error => alert(error.Message)
+      );
+    }
   }
 
   emailFormControl = new FormControl('', [
@@ -52,5 +97,25 @@ export class StudentComponent implements OnInit {
     Validators.email,
   ]);
 
+  FirstName = new FormControl('', [
+    Validators.required,
+  ]);
+
+  Mobile = new FormControl('', [
+    Validators.required,
+  ]);
+
+  LastName = new FormControl('', [
+    Validators.required,
+  ]);
+  DOB = new FormControl('', [
+    Validators.required
+  ]);
+  Gender = new FormControl('', [
+    Validators.required
+  ])
+  Address = new FormControl('', [
+    Validators.required
+  ]);
   matcher = new MyErrorStateMatcher();
 }
